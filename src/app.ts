ac45b3a23085a40f8c436836c78bc8d97db4f057
Jason Boyll <jason.boyll@gmail.com>
2016-11-17 20:49:29 -0500

    changed to use local data

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-const twitterLink: string = 'https://twitter.com/intent/tweet?hashtags=quotes&text=';
-const quoteURL: string = 'https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
+interface IQuoteData {
+    content: string;
+    title: string;
+}
 
-function getJSON(url: string, callback: Function): void {
-    let xhr: XMLHttpRequest = new XMLHttpRequest();
-    xhr.open('get', url, true);
+const twitterLink: string = 'https://twitter.com/intent/tweet?hashtags=quotes&text=';
+const quoteURL: string = 'data/quotes.json';
+let prevQuoteIndex: number = -1;
+let quoteData: Array<IQuoteData>; 
+
+function getJSON(url: string): void {
+    let xhr: XMLHttpRequest = new XMLHttpRequest(),
+        response: any;
+    xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onreadystatechange = function () {
         handleXhrResponse(xhr);
@@ -11,7 +19,7 @@ function getJSON(url: string, callback: Function): void {
     xhr.send();
 }
 
-function handleXhrResponse(xhr: XMLHttpRequest): void {
+function handleXhrResponse(xhr: XMLHttpRequest) {
     // 4 readState means the request is completed.
     if (4 != xhr.readyState) {
         // Do nothing if the request is not complete.
@@ -26,40 +34,50 @@ function handleXhrResponse(xhr: XMLHttpRequest): void {
         alert(`Error: status ${xhr.status}`);
     }
 
-    // Process the response if success.
-    updateQuote(xhr.response);   
+    // If no errors, load quote data and render initial quote
+    quoteData = xhr.response;
+    updateQuote();
 }
 
-function getQuote(): void {
-    getJSON(quoteURL, handleXhrResponse);
+function getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-interface IQuoteData {
-    quoteText: string;
-    quoteAuthor: string;
-}
-
-function updateQuote(quoteData: IQuoteData): void {
+function updateQuote(): void {
+    // Check quoteData is not null
+    if (quoteData === null || quoteData === undefined) {
+        alert('Something went wrong.');
+        return;
+    }
+    
     // Initialize local variables
-    let quote: string,
+    let index: number,
+        quote: string,
         author: string;
+
+    // Get a new random index
+    do {
+        index = getRandomInt(0, quoteData.length);
+    } while (index === prevQuoteIndex);
         
     // Get quote from quoteData
-    if (quoteData.hasOwnProperty('quoteText') &&
-        quoteData.hasOwnProperty('quoteAuthor')
+    if (quoteData[index].hasOwnProperty('content') &&
+        quoteData[index].hasOwnProperty('title')
     ) {
-        quote = quoteData.quoteText;
-        author = quoteData.quoteAuthor;
+        quote = quoteData[index].content;
+        author = quoteData[index].title;
         renderQuoteToDOM(quote, author);
         updateTweetLink(quote, author);
-    }    
+    }
 }
 
 function renderQuoteToDOM(quote: string, author: string): void {
     // Render quote data to DOM
     let elem: Element;
     elem = document.getElementById('quote');
-    elem.textContent = quote;
+    elem.innerHTML = quote;
     elem = document.getElementById('quote-author');
     elem.innerHTML = '<strong><em>-'.concat(author).concat('</em></strong>');
 }
@@ -67,9 +85,19 @@ function renderQuoteToDOM(quote: string, author: string): void {
 function updateTweetLink(quote: string, author: string): void {
     // Build and update Tweet link
     let elem: Element;
+    
+    // Strip HTML from quote string
+    elem = document.createElement('div');
+    elem.innerHTML = quote;
+    quote = elem.textContent;
+    
     elem = document.getElementById('tweet-button');
     elem.setAttribute("href", `${ twitterLink }${ quote } -${ author }`);
 }
 
-document.getElementById('button').addEventListener('click', getQuote, false);
-window.onload = getQuote;
+function init(): void {
+    getJSON(quoteURL);
+}
+
+document.getElementById('button').addEventListener('click', updateQuote, false);
+window.onload = init;
